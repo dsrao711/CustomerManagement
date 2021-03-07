f32cec090a24822bce4c9ec9b0e69b3bcae30daa
Divya <dsrao0712@gmail.com>
2021-03-07 17:52:42 +0530

    Migrations

--- accounts/models.py
+++ accounts/models.py
@@ -10,27 +10,27 @@ class Customer(models.Model) :
     def __str__ (self) :
         return self.name
 
-class Product (models.Model):
+class Product(models.Model):
+
     CATEGORY = (
         ('Indoor' , 'Indoor') ,
         ('Outdoor' , 'Outdoor')
     )
-    name = models.CharField(max_length= 200 , null= True)
-    price = models.FloatField(null= True)
-    category = models.CharField(max_length= 200 , null= True , categories = CATEGORY )
-    description = models.CharField(max_length= 200 , null= True)
-    date_created = models.DateTimeField(auto_now_add = True , null= True)
+    name = models.CharField(max_length = 200 , null = True) 
+    price = models.FloatField(max_length=200 , null=True)
+    description = models.CharField(max_length=200 , null = True , choices = CATEGORY )
+    date_created = models.DateTimeField(auto_now_add=True , null = True)
 
     def __str__ (self) :
         return self.name
 
-class Order(models.Model) :
+class Order (models.Model) :
     STATUS = (
-        ('Pending' , 'Pending') ,
-        ('Out for Delivery' , 'Out for Delivery') ,
-        ('Delivered' , 'Delivered')
+        ('Shipped' , 'Shipped'),
+        ('Out for delivery' , 'Out for delivery'),
+        ('Delivered' ,'Delivered')
     )
-    date_created = models.DateTimeField(auto_now_add = True , null= True)
-    status = models.CharField(max_length= 200 , null = True , category = STATUS )
-    def __str__ (self) : 
-        return self.product.name
+    status = models.CharField(max_length= 200 , null = True , choices = STATUS)
+    date_created = models.DateTimeField(auto_now_add= True, null = True)
+    def __str__ (self) :
+        return self.status
--- practice/settings.py
+++ practice/settings.py
@@ -33,12 +33,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts'
+  
 ]
 
 MIDDLEWARE = [
@@ -79,7 +82,6 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-        
     }
 }
 
